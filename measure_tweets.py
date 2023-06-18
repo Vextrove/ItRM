adc20251d05f3d5762c281848d4be4c6297f7ff7
Vextrove <52584544+Vextrove@users.noreply.github.com>
2023-06-18 12:17:12 +0200

    Create measure_tweets.py

---
+++ measure_tweets.py
@@ -0,0 +1,42 @@
+import os
+from collections import Counter
+
+
+def count_conjunctions(text):
+    '''Counts occurrences of each conjunction in a given list of words'''
+    conjunctions = Counter()
+    trigger_words = open("conjunctions.txt", "r").read().split()
+
+    for word in trigger_words:
+        conjunctions[word] = 0
+
+    for word in text:
+        if word in trigger_words:
+            conjunctions[word] += 1
+
+    return conjunctions
+
+
+def get_file_paths():
+    """Obtains file paths of tweets from a directory and its subdirectories"""
+    file_paths = []
+    for root, dirs, files in os.walk('tweets'):
+        for file in files:
+            if file.endswith('.pos'):
+                file_paths.append(os.path.join(root, file))
+    return file_paths
+
+    for file_path in get_file_paths():
+        with open(file_path, 'r') as file:
+            print(file)
+
+
+def main():
+    '''Runs the module with a test sentence containing a conjunction'''
+    text = "Deze zin bevat een conjunctie, waardoor iets zal gebeuren"
+    print(count_conjunctions(text.split()))
+
+
+if __name__ == "__main__":
+    """Only runs the main function if this code is run directly rather than imported"""
+    main()
