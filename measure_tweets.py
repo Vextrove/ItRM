15d28bda7e7d96124cffbdc445398b0c28065ea9
Vextrove <52584544+Vextrove@users.noreply.github.com>
2023-06-19 19:26:31 +0200

    Update measure_tweets.py

--- measure_tweets.py
+++ measure_tweets.py
@@ -89,8 +89,10 @@ def main():
     print('\nPercentage of tweets that contain at least one conjunction')
     print('Before 2017:', str(round(100 * hits_before / tweet_amount_before, 2)) + '%')
     print('After 2017:', str(round(100 * hits_after / tweet_amount_after, 2)) + '%')
-    print('Relative increase:',
+    print('Absolute increase:',
           str(round(100 * (hits_after / tweet_amount_after - hits_before / tweet_amount_before), 2)) + '%')
+    print('Relative increase:',
+          str(round(100 * (hits_after / tweet_amount_after) / (hits_before / tweet_amount_before), 2)) + '%')
 
     print('\nFrequency of each conjunction')
     print('Before 2017:', conjunctions_before)
